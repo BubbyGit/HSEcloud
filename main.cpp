36f1c423dae24e4b7ed89205df06c59fcd4aa1a3
BubbyGit <vishama@edu.hse.ru>
2024-06-12 21:28:02 +0300

    Final pj

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-/**
- * @file main.cpp
- * @brief Основной файл для приложения HSECloud Bot.
- */
-
-
 #include <tgbot/tgbot.h>
 #include <iostream>
 #include <memory>
@@ -18,30 +12,13 @@
 #define SQLITECPP_COMPILE_DLL
 #include <SQLiteCpp/SQLiteCpp.h>
 
-
 namespace fs = std::filesystem;
-
-/**
- * @brief Логирование сообщений.
- * 
- * Эта функция записывает сообщения в файл журнала с отметкой времени.
- * 
- * @param message Сообщение для записи в журнал.
- */
 
 void logMessage(const std::string& message) {
     std::ofstream logFile("bot.log", std::ios_base::app);
     std::time_t now = std::time(nullptr);
     logFile << std::ctime(&now) << ": " << message << std::endl;
-
 }
-/**
- * @brief Инициализация базы данных.
- * 
- * Эта функция создаёт SQLite базу данных и таблицу, если она не существует.
- * 
- * @param dbFileName Имя файла базы данных.
- */
 
 void initDatabase(const std::string& dbFileName) {
     try {
@@ -56,15 +33,6 @@ void initDatabase(const std::string& dbFileName) {
     }
 }
 
-/**
- * @brief Добавление пользователя в базу данных.
- * 
- * Эта функция вставляет нового пользователя в базу данных с начальным значением NULL для токена.
- * 
- * @param dbFileName Имя файла базы данных.
- * @param userId Идентификатор пользователя.
- */
-
 void addUserToDatabase(const std::string& dbFileName, int64_t userId) {
     try {
         SQLite::Database db(dbFileName, SQLite::OPEN_READWRITE);
@@ -78,14 +46,6 @@ void addUserToDatabase(const std::string& dbFileName, int64_t userId) {
         logMessage("Error adding user to database: " + std::string(e.what()));
     }
 }
-
-/**
- * @brief Генерация токена.
- * 
- * Эта функция генерирует случайный токен, состоящий из букв и цифр.
- * 
- * @return Сгенерированный токен.
- */
 
 std::string generateToken() {
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -102,16 +62,6 @@ std::string generateToken() {
     return token;
 }
 
-/**
- * @brief Обновление токена пользователя.
- * 
- * Эта функция обновляет токен пользователя в базе данных.
- * 
- * @param dbFileName Имя файла базы данных.
- * @param userId Идентификатор пользователя.
- * @param token Новый токен.
- */
-
 void updateUserToken(const std::string& dbFileName, int64_t userId, const std::string& token) {
     try {
         SQLite::Database db(dbFileName, SQLite::OPEN_READWRITE);
@@ -125,16 +75,6 @@ void updateUserToken(const std::string& dbFileName, int64_t userId, const std::s
         logMessage("Error updating user token: " + std::string(e.what()));
     }
 }
-
-/**
- * @brief Получение токена пользователя.
- * 
- * Эта функция извлекает токен пользователя из базы данных.
- * 
- * @param dbFileName Имя файла базы данных.
- * @param userId Идентификатор пользователя.
- * @return Токен пользователя.
- */
 
 std::string getUserToken(const std::string& dbFileName, int64_t userId) {
     try {
@@ -154,14 +94,6 @@ std::string getUserToken(const std::string& dbFileName, int64_t userId) {
     return "";
 }
 
-/**
- * @brief Создание папки для пользователя.
- * 
- * Эта функция создаёт директорию для хранения файлов пользователя.
- * 
- * @param token Токен пользователя.
- */
-
 void createFolderForUser(const std::string& token) {
     try {
         std::string path = "C:/Users/Public/Music/CloudBot/main/files/" + token;
@@ -173,15 +105,6 @@ void createFolderForUser(const std::string& token) {
     }
 }
 
-/**
- * @brief Получение списка файлов.
- * 
- * Эта функция возвращает список файлов в указанной директории.
- * 
- * @param folder_path Путь к директории.
- * @return Вектор с именами файлов.
- */
-
 std::vector<std::string> get_files(const std::string& folder_path) {
     std::vector<std::string> file_list;
     for (const auto& entry : fs::directory_iterator(folder_path)) {
@@ -191,32 +114,12 @@ std::vector<std::string> get_files(const std::string& folder_path) {
     return file_list;
 }
 
-/**
- * @brief Проверка токена.
- * 
- * Эта функция проверяет существование директории для данного токена.
- * 
- * @param token Токен пользователя.
- * @return true, если директория существует и является директорией.
- * @return false, если директория не существует или не является директорией.
- */
-
 bool validate_token(const std::string& token) {
     std::string token_folder = "C:/Users/Public/Music/CloudBot/main/files/" + token;
     bool isValid = fs::exists(token_folder) && fs::is_directory(token_folder);
     logMessage("Token validation: " + token + " is " + (isValid ? "valid" : "invalid"));
     return isValid;
 }
-
-/**
- * @brief Генерация HTML-списка файлов.
- * 
- * Эта функция создаёт HTML-код для отображения списка файлов.
- * 
- * @param files Вектор с именами файлов.
- * @param token Токен пользователя.
- * @return Строка с HTML-кодом списка файлов.
- */
 
 std::string generate_file_list_html(const std::vector<std::string>& files, const std::string& token) {
     std::stringstream ss;
@@ -227,11 +130,88 @@ std::string generate_file_list_html(const std::vector<std::string>& files, const
     return ss.str();
 }
 
-/**
- * @brief Запуск сервера.
- * 
- * Эта функция запускает HTTP-сервер, который обрабатывает запросы на загрузку и скачивание файлов.
- */
+std::string generate_send_token(size_t length) {
+    const char charset[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    std::default_random_engine rng(std::random_device{}());
+    std::uniform_int_distribution<> dist(0, sizeof(charset) - 2);
+
+    std::string token;
+    for (size_t i = 0; i < length; ++i) {
+        token += charset[dist(rng)];
+    }
+    return token;
+}
+
+void handle_file_upload(const httplib::Request& req, httplib::Response& res) {
+    std::string token = generate_send_token(12);
+    std::string dir_path = "hidefiles/" + token;
+    fs::create_directories(dir_path);
+
+    std::vector<std::string> filenames;
+
+    for (const auto& file : req.files) {
+        auto& file_info = file.second;
+        std::string file_path = dir_path + "/" + file_info.filename;
+        std::ofstream ofs(file_path, std::ios::binary);
+        ofs << file_info.content;
+        ofs.close();
+        filenames.push_back(file_info.filename);
+    }
+
+    std::string link = "http://localhost:8080/sendfile/" + token;
+    std::string response = "{\"link\": \"" + link + "\", \"files\": [";
+    for (size_t i = 0; i < filenames.size(); ++i) {
+        response += "\"" + filenames[i] + "\"";
+        if (i < filenames.size() - 1) response += ", ";
+    }
+    response += "]}";
+
+    res.set_content(response, "application/json");
+}
+
+void handle_file_download_page(const httplib::Request& req, httplib::Response& res) {
+    std::string token = req.matches[1].str();
+    std::string dir_path = "hidefiles/" + token;
+
+    if (!fs::exists(dir_path) || !fs::is_directory(dir_path)) {
+        res.set_content("Files not found", "text/plain");
+        res.status = 404;
+        return;
+    }
+
+    std::string html = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Download Files</title><style>body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; height: 100vh; } .container { background: #fff; padding: 20px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); border-radius: 8px; text-align: center; width: 80%; max-width: 600px; } ul { list-style-type: none; padding: 0; } li { margin: 10px 0; background: #e9ecef; padding: 10px; border-radius: 4px; } a { text-decoration: none; color: #007BFF; } a:hover { text-decoration: underline; }</style></head><body><div class=\"container\"><h1>Download Files</h1><ul>";
+
+    for (const auto& entry : fs::directory_iterator(dir_path)) {
+        std::string filename = entry.path().filename().string();
+        std::string file_link = "/sendfile/" + token + "/" + filename;
+        html += "<li><a href=\"" + file_link + "\">" + filename + "</a></li>";
+    }
+
+    html += "</ul></div></body></html>";
+
+    res.set_content(html, "text/html");
+}
+
+void handle_file_download(const httplib::Request& req, httplib::Response& res) {
+    std::string token = req.matches[1].str();
+    std::string filename = req.matches[2].str();
+    std::string file_path = "hidefiles/" + token + "/" + filename;
+
+    if (fs::exists(file_path) && fs::is_regular_file(file_path)) {
+        std::ifstream ifs(file_path, std::ios::binary);
+        std::ostringstream oss;
+        oss << ifs.rdbuf();
+        res.set_content(oss.str(), "application/octet-stream");
+        res.set_header("Content-Disposition", "attachment; filename=" + filename);
+    }
+    else {
+        res.set_content("File not found", "text/plain");
+        res.status = 404;
+    }
+}
 
 void startServer() {
     httplib::Server svr;
@@ -285,18 +265,19 @@ void startServer() {
         }
         });
 
-    svr.listen("localhost", 8080);
-    logMessage("Server started on port 8080");
-}
+    svr.Get("/sendfile", [](const httplib::Request&, httplib::Response& res) {
+        std::ifstream ifs("sendfiles.html");
+        std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+        res.set_content(content, "text/html");
+        });
 
-/**
- * @brief Основная функция приложения.
- * 
- * Эта функция инициализирует базу данных, настраивает бота Telegram, запускает сервер
- * и обрабатывает команды и события от пользователей.
- * 
- * @return Код завершения программы.
- */
+    svr.Post("/upload", handle_file_upload);
+    svr.Get(R"(/sendfile/([0-9A-Za-z]+))", handle_file_download_page);
+    svr.Get(R"(/sendfile/([0-9A-Za-z]+)/(.+))", handle_file_download);
+
+    logMessage("Server started on port 8080");
+    svr.listen("0.0.0.0", 8080);
+}
 
 int main() {
     std::thread serverThread(startServer);
@@ -329,6 +310,11 @@ int main() {
         uploadButton->text = "Upload";
         uploadButton->callbackData = "upload";
         row.push_back(uploadButton);
+
+        TgBot::InlineKeyboardButton::Ptr sendButton(new TgBot::InlineKeyboardButton);
+        sendButton->text = "Send";
+        sendButton->callbackData = "send";
+        row.push_back(sendButton);
 
         keyboard->inlineKeyboard.push_back(row);
 
@@ -381,7 +367,7 @@ int main() {
 
                 TgBot::InlineKeyboardButton::Ptr webAppButton(new TgBot::InlineKeyboardButton);
                 webAppButton->text = "Open WebApp";
-                webAppButton->url = "https://0b38-62-217-184-150.ngrok-free.app";
+                webAppButton->url = "https://235f-62-217-184-150.ngrok-free.app";
                 row.push_back(webAppButton);
 
                 keyboard->inlineKeyboard.push_back(row);
@@ -390,18 +376,24 @@ int main() {
                 logMessage("Sent web app link to user. UserID: " + std::to_string(query->message->chat->id));
             }
         }
+        else if (query->data == "send") {
+            TgBot::InlineKeyboardMarkup::Ptr keyboard(new TgBot::InlineKeyboardMarkup);
+            std::vector<TgBot::InlineKeyboardButton::Ptr> row;
+
+            TgBot::InlineKeyboardButton::Ptr webAppButton(new TgBot::InlineKeyboardButton);
+            webAppButton->text = "Open WebApp";
+            webAppButton->url = "https://235f-62-217-184-150.ngrok-free.app/sendfile";
+            row.push_back(webAppButton);
+
+            keyboard->inlineKeyboard.push_back(row);
+
+            bot.getApi().sendMessage(query->message->chat->id, "Click the button below to open the web application.", false, 0, keyboard);
+            logMessage("Sent web app link to user. UserID: " + std::to_string(query->message->chat->id));
+        }
         bot.getApi().answerCallbackQuery(query->id);
         });
 
     TgBot::TgLongPoll longPoll(bot);
-
-    /**
- * @brief Логирование сообщений.
- * 
- * Эта функция записывает сообщения в файл журнала с отметкой времени.
- * 
- * @param message Сообщение для записи в журнал.
- */
 
     logMessage("Bot started...");
     while (true) {
