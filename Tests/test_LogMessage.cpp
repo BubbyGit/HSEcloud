6435c5a26aede233c07e4bacc9abeafd70bea73c
Th-alexm <alexmad0607@gmail.com>
2024-06-19 12:00:09 +0300

    Tests v2

--- Tests/test_LogMessage.cpp
+++ Tests/test_LogMessage.cpp
@@ -3,9 +3,8 @@
 #include <iostream>
 #include "functions.h"
 
-void testLogMessage() {
-    std::string testMessage = "Test log message";
-    logMessage(testMessage);
-
-    std::ifstream logFile(getCurrentDir() + "/bot.log");
-    std::
+void logMessage(const std::string& message) {
+    std::ofstream logFile(BASE_PATH + "/bot.log", std::ios_base::app);
+    std::time_t now = std::time(nullptr);
+    logFile << std::ctime(&now) << ": " << message << std::endl;
+}
