907466aa8dfb53145085f4793bd5515ec9f73aa3
Th-alexm <alexmad0607@gmail.com>
2024-06-27 12:49:51 +0300

    Test

--- Tests/test_LogMessage.cpp
+++ Tests/test_LogMessage.cpp
@@ -3,8 +3,42 @@
 #include <iostream>
 #include "functions.h"
 
-void logMessage(const std::string& message) {
-    std::ofstream logFile(BASE_PATH + "/bot.log", std::ios_base::app);
-    std::time_t now = std::time(nullptr);
-    logFile << std::ctime(&now) << ": " << message << std::endl;
+void testGetCurrentDir() {
+    std::string dir = getCurrentDir();
+    assert(!dir.empty());
+    std::cout << "Current Directory: " << dir << std::endl;
+}
+
+void testLogMessage() {
+    std::string testMessage = "Test log message";
+    logMessage(testMessage);
+
+    std::ifstream logFile(getCurrentDir() + "/bot.log");
+    std::string lastLine;
+    while (logFile.good()) {
+        std::string line;
+        std::getline(logFile, line);
+        if (!line.empty()) {
+            lastLine = line;
+        }
+    }
+    logFile.close();
+
+    assert(lastLine.find(testMessage) != std::string::npos);
+    std::cout << "Log Message Test Passed" << std::endl;
+}
+
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        std::string testName = argv[1];
+        if (testName == "testGetCurrentDir") {
+            testGetCurrentDir();
+        } else if (testName == "testLogMessage") {
+            testLogMessage();
+        }
+    } else {
+        std::cerr << "No test specified." << std::endl;
+        return 1;
+    }
+    return 0;
 }
